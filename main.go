ebe6da2a92e281a6a1861b1efb902ccb29d66b2a
Agniva De Sarker <agnivade@yahoo.co.in>
2023-03-30 11:33:56 +0530

    MM-51702: Display Performance Insights metrics
    - We also do some preliminary refactoring.
    - Filled some context timeouts.
    
    More refactoring to happen.
    
    https://mattermost.atlassian.net/browse/MM-51702

--- main.go
+++ main.go
@@ -7,24 +7,36 @@ import (
 	"log"
 	"os"
 	"path"
+	"sort"
 	"strconv"
 	"strings"
 	"sync"
 	"time"
 
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/pi"
+	"github.com/aws/aws-sdk-go/service/rds"
 	"github.com/jackc/pgx/v5"
 	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/mattermost/morph/models"
 )
 
-const queryTimeout = time.Hour
+const (
+	queryTimeout     = time.Hour
+	statQueryTimeout = 30 * time.Second
+)
 
 func main() {
 	var startingMigration int
-	var sourceDir, dsn string
+	var sourceDir, dsn, awsAccessKey, awsSecretKey, awsRegion string
 	flag.IntVar(&startingMigration, "start", 0, "Starting migration number from where to apply migrations")
 	flag.StringVar(&sourceDir, "source_dir", "", "Source directory containing the migrations. Example: ~/mattermost-server/server/channels/db/migrations/postgres")
 	flag.StringVar(&dsn, "dsn", "postgres://mmuser:mostest@localhost/mattermost_test?sslmode=disable", "Database DSN to connect to")
+	flag.StringVar(&awsAccessKey, "access_key", "", "AWS Access Key")
+	flag.StringVar(&awsSecretKey, "secret_key", "", "AWS Secret Key")
+	flag.StringVar(&awsRegion, "region", "", "AWS region")
 	flag.Parse()
 
 	var confirm string
@@ -44,6 +56,17 @@ func main() {
 
 	if startingMigration == 0 {
 		logger.Println("-start not passed. Please use -h to see all the flags")
+		return
+	}
+
+	creds := credentials.NewStaticCredentials(awsAccessKey, awsSecretKey, "")
+
+	sess, err := session.NewSession(&aws.Config{
+		Region:      aws.String(awsRegion),
+		Credentials: creds,
+	})
+	if err != nil {
+		logger.Printf("Error initializing AWS session: %v\n", err)
 		return
 	}
 
@@ -68,12 +91,15 @@ func main() {
 	}
 	defer pool.Close()
 
-	_, err = pool.Exec(context.TODO(), "CREATE EXTENSION IF NOT EXISTS pg_stat_statements;")
+	ctx, cancel := context.WithTimeout(context.Background(), statQueryTimeout)
+	defer cancel()
+	_, err = pool.Exec(ctx, "CREATE EXTENSION IF NOT EXISTS pg_stat_statements;")
 	if err != nil {
 		logger.Println(err)
 		return
 	}
 
+	start := time.Now()
 	for _, entry := range entries {
 		if strings.HasSuffix(entry.Name(), ".down.sql") {
 			continue
@@ -95,11 +121,16 @@ func main() {
 			continue
 		}
 
-		_, err = pool.Exec(context.TODO(), "SELECT pg_stat_statements_reset();")
+		resetCtx, resetCancel := context.WithTimeout(context.Background(), statQueryTimeout)
+		// Not using defer cancel() because this is running in a loop,
+		// and defer won't be called until the cmd exits.
+		_, err = pool.Exec(resetCtx, "SELECT pg_stat_statements_reset();")
 		if err != nil {
+			resetCancel()
 			logger.Println(err)
 			return
 		}
+		resetCancel()
 
 		fmt.Println(strings.Repeat("-", 100))
 		fmt.Printf("Starting migration: %s\n", entry.Name())
@@ -113,46 +144,7 @@ func main() {
 		quit := make(chan struct{})
 		var wg sync.WaitGroup
 		wg.Add(1)
-		go func() {
-			defer wg.Done()
-			ticker := time.NewTicker(time.Second)
-			defer ticker.Stop()
-
-			// Because we are running the query in a loop, we might get the same locked
-			// tables over and over again. This is to get the result of only unique tables.
-			lockMap := make(map[string]bool)
-
-			defer func() {
-				fmt.Println("LOCKS:")
-				for k := range lockMap {
-					split := strings.Split(k, "-")
-					fmt.Printf("LOCK TYPE: %s, RELATION: %s, MODE: %s, GRANTED: %s\n", split[0], split[1], split[2], split[3])
-				}
-			}()
-
-			for {
-				select {
-				case <-quit:
-					return
-				case <-ticker.C:
-					rows, err := pool.Query(context.TODO(), `SELECT locktype, relation::regclass, mode, granted
-				FROM pg_catalog.pg_locks l
-				LEFT JOIN pg_catalog.pg_database db ON db.oid = l.database
-				JOIN pg_class c ON l.relation=c.oid
-				WHERE (db.datname = current_database() OR db.datname IS NULL) AND c.relkind='r' AND NOT pid = pg_backend_pid() AND locktype='relation';`)
-					if err != nil {
-						logger.Println(err)
-						return
-					}
-					var lockType, relation, mode string
-					var granted bool
-					pgx.ForEachRow(rows, []any{&lockType, &relation, &mode, &granted}, func() error {
-						lockMap[fmt.Sprintf("%s-%s-%s-%t", lockType, relation, mode, granted)] = true
-						return nil
-					})
-				}
-			}
-		}()
+		go checkLocks(&wg, quit, logger, pool)
 
 		_, err = pool.Exec(context.TODO(), string(buf))
 		if err != nil {
@@ -186,5 +178,146 @@ func main() {
 			return nil
 		})
 		cancel()
+	}
+	end := time.Now()
+	fmt.Println(strings.Repeat("-", 100))
+	printPerfMetrics(end, start, sess, logger)
+}
+
+// TODO: refactor this to a method.
+func checkLocks(wg *sync.WaitGroup, quit chan struct{}, logger *log.Logger, pool *pgxpool.Pool) {
+	defer wg.Done()
+	ticker := time.NewTicker(time.Second)
+	defer ticker.Stop()
+
+	// Because we are running the query in a loop, we might get the same locked
+	// tables over and over again. This is to get the result of only unique tables.
+	lockMap := make(map[string]bool)
+
+	defer func() {
+		fmt.Println("LOCKS:")
+		for k := range lockMap {
+			split := strings.Split(k, "-")
+			fmt.Printf("LOCK TYPE: %s, RELATION: %s, MODE: %s, GRANTED: %s\n", split[0], split[1], split[2], split[3])
+		}
+	}()
+
+	for {
+		select {
+		case <-quit:
+			return
+		case <-ticker.C:
+			rows, err := pool.Query(context.TODO(), `SELECT locktype, relation::regclass, mode, granted
+				FROM pg_catalog.pg_locks l
+				LEFT JOIN pg_catalog.pg_database db ON db.oid = l.database
+				JOIN pg_class c ON l.relation=c.oid
+				WHERE (db.datname = current_database() OR db.datname IS NULL) AND c.relkind='r' AND NOT pid = pg_backend_pid() AND locktype='relation';`)
+			if err != nil {
+				logger.Println(err)
+				return
+			}
+			var lockType, relation, mode string
+			var granted bool
+			pgx.ForEachRow(rows, []any{&lockType, &relation, &mode, &granted}, func() error {
+				lockMap[fmt.Sprintf("%s-%s-%s-%t", lockType, relation, mode, granted)] = true
+				return nil
+			})
+		}
+	}
+}
+
+const (
+	metricCPUUtil    = "os.cpuUtilization.total.max"
+	metricReadKBPS   = "os.diskIO.rdstemp.readKbPS.max"
+	metricWriteKBPS  = "os.diskIO.rdstemp.writeKbPS.max"
+	metricActiveMem  = "os.memory.active.max"
+	metricBuffersMem = "os.memory.buffers.max"
+)
+
+// TODO: covert these into methods with logger, sess as a struct field.
+func printPerfMetrics(end, start time.Time, sess *session.Session, logger *log.Logger) {
+	if end.Sub(start) <= time.Minute {
+		fmt.Println("Total runtime was less than a minute. Not capturing any DB metrics.")
+		return
+	}
+
+	rdsSvc := rds.New(sess)
+	instances, err := rdsSvc.DescribeDBInstances(&rds.DescribeDBInstancesInput{})
+	if err != nil {
+		logger.Println(err)
+		return
+	}
+	// TODO: pagination
+	fmt.Println("Choose the DB instance to display metrics from:")
+	instanceMap := make(map[int]string)
+	for i, inst := range instances.DBInstances {
+		instanceMap[i] = *inst.DbiResourceId
+		fmt.Printf("[%d] Cluster Name: %s, Instance Name: %s, Endpoint: %s\n", i, *inst.DBClusterIdentifier, *inst.DBInstanceIdentifier, *inst.Endpoint.Address)
+	}
+	var iNum int
+	fmt.Print("Enter the number: ")
+	fmt.Scanln(&iNum)
+
+	perfI := pi.New(sess)
+	fmt.Println("METRICS:")
+	out, err := perfI.GetResourceMetrics(&pi.GetResourceMetricsInput{
+		StartTime:       &start,
+		EndTime:         &end,
+		ServiceType:     aws.String(pi.ServiceTypeRds),
+		Identifier:      aws.String(instanceMap[iNum]),
+		PeriodInSeconds: aws.Int64(60),
+		MetricQueries: []*pi.MetricQuery{
+			{
+				Metric: aws.String(metricCPUUtil),
+			},
+			{
+				Metric: aws.String(metricReadKBPS),
+			},
+			{
+				Metric: aws.String(metricWriteKBPS),
+			},
+			{
+				Metric: aws.String(metricActiveMem),
+			},
+			{
+				Metric: aws.String(metricBuffersMem),
+			},
+		},
+	})
+	if err != nil {
+		logger.Println(err)
+		return
+	}
+	// TODO: pagination
+	// This part of the code just re-arranges the output in a way that
+	// can be printed chronologically. The current output returns a list
+	// of metric results, where each array item contains all the data points.
+	// We change that to a map keyed by the timestamp, and the value of which
+	// is another map which contains the metric name and the measurement for that
+	// timestamp.
+	metricStore := make(map[int64]map[string]float64)
+	for _, metric := range out.MetricList {
+		name := *metric.Key.Metric
+		for _, point := range metric.DataPoints {
+			ts := point.Timestamp.Unix()
+			if metricStore[ts] == nil {
+				metricStore[ts] = make(map[string]float64)
+			}
+			metricStore[ts][name] = *point.Value
+		}
+	}
+	tsSlice := make([]int64, 0, len(metricStore))
+	for ts := range metricStore {
+		tsSlice = append(tsSlice, ts)
+	}
+	sort.Slice(tsSlice, func(i, j int) bool { return tsSlice[i] < tsSlice[j] })
+
+	// ts					metric metric metric
+	// 2023-03-30 13:37:00	val		val		val
+	fmt.Printf("%25s %25s %25s %25s %25s %25s\n", "Time", metricCPUUtil, metricReadKBPS, metricWriteKBPS, metricActiveMem, metricBuffersMem)
+	for _, ts := range tsSlice {
+		fromUnixT := time.Unix(ts, 0)
+		metricMap := metricStore[ts]
+		fmt.Printf("%25v %25f %25f %25f %25f %25f\n", fromUnixT, metricMap[metricCPUUtil], metricMap[metricReadKBPS], metricMap[metricWriteKBPS], metricMap[metricActiveMem], metricMap[metricBuffersMem])
 	}
 }
